e6309cccc87f04861bee60f04ddaa239ae311e6c
Arun Seehra <seehra.a@gmail.com>
2018-10-01 10:58:28 -0500

    Create addItem method on the store

--- scripts/store.js
+++ scripts/store.js
@@ -1,15 +1,36 @@
 'use strict';
 
+/* global Item */
+
 // eslint-disable-next-line no-unused-vars
 const store = (function() {
-    const items = [
-        // { id: cuid(), name: 'apples', checked: false },
-        // { id: cuid(), name: 'oranges', checked: false },
-        // { id: cuid(), name: 'milk', checked: true },
-        // { id: cuid(), name: 'bread', checked: false }
-      ];
-      const hideCheckedItems = false;
-      const searchTerm = '';
-    return { items, hideCheckedItems, searchTerm };
+  const items = [
+    // { id: cuid(), name: 'apples', checked: false },
+    // { id: cuid(), name: 'oranges', checked: false },
+    // { id: cuid(), name: 'milk', checked: true },
+    // { id: cuid(), name: 'bread', checked: false }
+  ];
+  const hideCheckedItems = false;
+  const searchTerm = '';
 
-}() );
+  function findById(id) {
+    return this.items.find(item => id === item.id);
+  }
+
+  function addItem(name) {
+    try {
+      Item.validateName(name);
+      this.items.push(Item.create(name));
+    } catch (error) {
+      console.error(`Could not add item: ${error.message}`);
+    }
+  }
+
+  return {
+    items,
+    hideCheckedItems,
+    searchTerm,
+    findById,
+    addItem
+  };
+})();
