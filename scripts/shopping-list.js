87c8a8ccbd6380dede01b1478f009b35900a32d1
Arun Seehra <seehra.a@gmail.com>
2018-10-01 10:50:34 -0500

    Use factory to add new shopping items

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -1,4 +1,5 @@
-/* global store, cuid */
+'use strict';
+/* global store, Item */
 
 // eslint-disable-next-line no-unused-vars
 const shoppingList = (function(){
@@ -56,7 +57,13 @@ const shoppingList = (function(){
   
   
   function addItemToShoppingList(itemName) {
-    store.items.push({ id: cuid(), name: itemName, checked: false });
+    try {
+      Item.validateName(itemName);
+      store.items.push(Item.create(itemName));
+      render();
+    } catch (error) {
+      console.error(`Cannot add item: ${error.message}`);
+    }
   }
   
   function handleNewItemSubmit() {
