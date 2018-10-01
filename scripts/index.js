ba3d45750bcde3b6cb4e9f1bf3539b70c594d933
Arun Seehra <seehra.a@gmail.com>
2018-10-01 10:45:15 -0500

    Remove testing code

--- scripts/index.js
+++ scripts/index.js
@@ -4,14 +4,5 @@
 
 $(document).ready(function() {
   shoppingList.bindEventListeners();
-  const itemNames = ['', 'apples', 'pears'];
-  itemNames.forEach(name => {
-    try {
-      Item.validateName(name);
-      store.items.push(Item.create(name));
-    } catch (error) {
-      console.log('Cannot add item: ' + error.message);
-    }
-  });
   shoppingList.render();
 });
